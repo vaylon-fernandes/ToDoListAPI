4597162fcd9e2e2338b7c092a6c45bcc2488e6b8
vaylon-fernandes <vaylonfernandes@gmail.com>
2024-08-05 09:23:26 +0530

    Add project files.

---
+++ ToDoListAPI/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDoListAPI.Models;
+
+    
+
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string status { get; set; } = "To Do";
+        
+        public DateTime createdAt { get; set; } = DateTime.Now;
+    }
+
